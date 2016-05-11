c9c975c8a41b5f3e59b26217f957812ad6436f7f
Jihen <tekaya.jihen.91@gmail.com>
2016-05-11 10:29:47 +0100

    First commit

---
+++ www/js/app.js
@@ -0,0 +1,63 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'IncidentsModule'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+      url: '/app',
+      abstract: true,
+      templateUrl: 'templates/menu/menu.html'
+    })
+
+    .state('app.about', {
+      url: '/about',
+      views: {
+        'content': {
+          templateUrl: 'templates/main/about.html'
+        }
+      }
+    })
+
+    .state('app.new', {
+      url: '/new',
+      views: {
+        'content': {
+          templateUrl: 'templates/new/new.html'
+        }
+      }
+    })
+
+    .state('app.login', {
+      url: '/login',
+      views: {
+        'content': {
+          templateUrl: 'templates/login/login.html'
+        }
+      }
+    });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/login');
+});
---
+++ www/js/incidents/Incidents/IncidentsController.js
@@ -0,0 +1,15 @@
+angular.module('IncidentsModule')
+
+.controller('IncidentsController', function($scope, Incident, incidents) {
+
+    var initView = function() {
+
+        $scope.incidents = incidents;
+
+    };
+
+    $scope.$on('$ionicView.loaded', function() {
+        initView();
+    });
+
+});
